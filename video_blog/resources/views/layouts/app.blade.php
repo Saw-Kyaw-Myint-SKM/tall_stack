9d56c7ef7bdf8eb75d652d81e535014442d791d0
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-01-21 20:23:58 +0630

    fix design bug

--- video_blog/resources/views/layouts/app.blade.php
+++ video_blog/resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link href="https://vjs.zencdn.net/8.9.0/video-js.css" rel="stylesheet" />
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -34,10 +33,7 @@
             {{ $slot }}
         </main>
     </div>
-    <script>
-        <script src = "https://vjs.zencdn.net/8.9.0/video.min.js" >
-    </script>
-    </script>
+    <script src="https://vjs.zencdn.net/8.9.0/video.min.js"></script>
 </body>
 
 </html>
--- video_blog/resources/views/livewire/pages/videos/show.blade.php
+++ video_blog/resources/views/livewire/pages/videos/show.blade.php
@@ -15,10 +15,14 @@ new #[Layout('layouts.app')] class extends Component
 ?>
 
 <div class="max-w-4xl mx-auto">
-    <video id="my-video" class="video-js  w-full h-auto" controls preload="auto" poster="{{ asset($blog->poster) }}" data-setup="{}"
-        controls>
-        <source src="{{ asset($blog->video) }}" type="video/mp4">
-        <source src="movie.ogg" type="video/ogg">
-        Your browser does not support the video tag.
-    </video>
+    <div id='test'>
+        <video id="myvideo" class="video-js vjs-fluid w-full h-auto" controls preload="auto"
+            poster="{{ asset($blog->poster) }}" data-setup='{}'>
+            <source src="{{ asset($blog->video) }}" type="video/mp4">
+        </video>
+    </div>
 </div>
+@assets
+    <link href="https://vjs.zencdn.net/8.9.0/video-js.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/video-js-skin.css') }}">
+@endassets
